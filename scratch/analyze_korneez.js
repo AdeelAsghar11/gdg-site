9b48b18b1c057049e5e81a9c2b50b8f512a6cf22
Umm e Habiba <syedaummehabiba52@gmail.com>
2026-09-12 21:47:31 +0500

    Remove private testing guide and untrack temporary scratch files

--- scratch/analyze_korneez.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-// We can read raw pixels by converting to uncompressed or checking with a small script
-console.log('Korneez raw image exists:', fs.existsSync('public/partners/korneez_raw.jpg'));
-fs.copyFileSync('public/partners/korneez_raw.jpg', 'public/partners/korneez.jpg');
-fs.copyFileSync('public/partners/korneez_raw.jpg', 'public/partners/korneez.png');
-console.log('Copied to public/partners/korneez.jpg and public/partners/korneez.png');
--- scratch/check_hosts_hackdata.js
+++
@@ -1,9 +0,0 @@
-async function check() {
-  const res = await fetch('http://localhost:3000/events/hack-data-v1');
-  const text = await res.text();
-  const start = text.indexOf('Event Speakers');
-  if (start !== -1) {
-    console.log(text.slice(start, start + 1000));
-  }
-}
-check();
